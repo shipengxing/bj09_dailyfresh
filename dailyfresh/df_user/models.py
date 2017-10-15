ac4332dbf7f866ef1e4543ca6f1ffc2c265d28f2
shipeng <1223964095@qq.com>
2017-10-15 16:08:02 +0800

    创建df_user应用

---
+++ dailyfresh/df_user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Userinfo(models.Model):
+    username = models.CharField(max_length =20,verbose_name='用户名')
+    password = models.CharField(max_length =20,verbose_name='密码')
+    def __str__(self):
+        return self.username
+
